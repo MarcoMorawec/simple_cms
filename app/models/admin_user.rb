df36e816bea870661948d0ba39c3dc236c615e51
MarcoMorawec <marco.morawec@gmail.com>
2013-06-06 09:01:56 -0400

    adding associations to page and AdminUser models, and creating the table in the migration file for the join table

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -1,6 +1,10 @@
 class AdminUser < ActiveRecord::Base
   attr_accessible :first_name, :last_name, :email, :username
 
+  #adding the relationsships between AdminUser - admin_users_pages (join table) - Pages
+  has_and_belongs_to_many :pages
+
+
   scope :search_first_name, lambda {|query| where(["first_name LIKE ?", "%#{query}%"])}
   scope :search_name, lambda {|first, last| where(:first_name => first, :last_name => last)}
 
