98e83265a1af43ba5090497f35107137e0d4366c
MarcoMorawec <marco.morawec@gmail.com>
2013-06-06 09:07:02 -0400

    changing the association from page to AdminUser to :editor and pointing rails to the class AdminUser

--- app/models/page.rb
+++ app/models/page.rb
@@ -4,7 +4,9 @@ class Page < ActiveRecord::Base
    belongs_to :subject
    has_many :sections
 
-   has_and_belongs_to_many :admin_users
+   #going against rails naming convention here, but telling rails that by editors 
+   #we mean actually AdminUsers in the class => AdminUser
+   has_and_belongs_to_many :editors, :class => "AdminUser"
 
 
 end
