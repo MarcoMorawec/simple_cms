80139b6b2c753c8120fcf0358c805bc93c943c59
MarcoMorawec <marco.morawec@gmail.com>
2013-06-12 08:47:53 -0400

    adding index default action and show action to subject controller and showing detailed subject data in show.html

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -1,7 +1,18 @@
 class SubjectsController < ApplicationController
 
+	#force our list page to render in case somebody goes directly to the url ./subjects (otherwise we'd get an error)
+	def index
+		list
+		render ('list')
+	end
+
+
 	def list
-		@subject = Subject.order("subjects.position ASC")
+		@subjects = Subject.order("subjects.position ASC")
+	end
+
+	def show
+		@subject = Subject.find(params[:id])
 	end
 
 end
