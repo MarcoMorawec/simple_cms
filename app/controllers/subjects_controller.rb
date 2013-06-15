cace36d9dc7b2679fd9bcfba01f5e5ed3702a166
MarcoMorawec <marco.morawec@gmail.com>
2013-06-14 22:31:19 -0400

    adding the create action to the controller so the form processes and saves to the database

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -20,6 +20,21 @@ class SubjectsController < ApplicationController
 		@subject = Subject.new(:name => 'Enter the Name', :position => "Enter the position")
 	end
 
+	def create
+	#instantiate a new object using form parameters
+		@subject = Subject.new(params[:subject])
+	#save the object
+		if @subject.save
+			#if save succeeds display list view
+			redirect_to(:action => 'list')
+			#if save fails display form again
+		else
+			render('new')
+		end
+	end
+
 	
+
+
 
 end
