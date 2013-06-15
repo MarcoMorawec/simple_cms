dbc777c35c1c1319de228f5a6983cd53d80f100b
MarcoMorawec <marco.morawec@gmail.com>
2013-06-15 00:02:54 -0400

    adding edit and update actions to the subject class and adding an edit.html.erb  view

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -33,7 +33,24 @@ class SubjectsController < ApplicationController
 		end
 	end
 
-	
+	def edit
+		#find by ID
+		@subject = Subject.find(params[:id])
+
+	end
+		
+	def update
+	#find object to update using form parameters
+		@subject = Subject.find(params[:id])
+	#update the object
+		if @subject.update_attributes(params[:subject])
+			#if update succeeds display list view
+			redirect_to(:action => 'show', :id => @subject.id)
+			#if update fails display form again
+		else
+			render('edit')
+		end
+	end
 
 
 
