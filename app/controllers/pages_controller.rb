ab80d2a3bbc61f1a87902ea532470590aa75d3ef
MarcoMorawec <marco.morawec@gmail.com>
2013-06-17 08:05:01 -0400

    adding list and show action to pages conroller and list and show view

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,2 +1,25 @@
 class PagesController < ApplicationController
+
+	#def index
+
+	def list
+		@page = Page.order("pages.position ASC")
+	end
+
+	def show
+		@page = Page.find(params[:id])
+	end
+
+
+
+
+
+
+
+
+
+
+
+
+
 end
