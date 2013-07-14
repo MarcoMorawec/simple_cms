706d860548a0389ebfa75a8907cb7eda5e93accb
marcomorawec <marco.morawec@gmail.com>
2013-07-14 14:01:10 -0400

    adding styling helper methods to the application helper file

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+
+	def status_tag(boolean, options={})
+		options[:true] ||= 'Yes'
+		options[:true_class] ||= 'status_true'
+		options[:false] ||= 'No'
+		options[:false_class] ||= 'status_false'
+
+		if boolean
+			content_tag(:span, options[:true], :class => options[:true_class])
+		else
+			content_tag(:span, options[:false], :class => options[:false_class])
+		end
+	end
+
+
+
 end
